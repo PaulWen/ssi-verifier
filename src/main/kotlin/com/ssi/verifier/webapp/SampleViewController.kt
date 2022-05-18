cdc10e35a716c5d84456f193aac49ba70a6e8def
PaulWen <wenzel.pgb@googlemail.com>
2022-05-18 13:41:56 +0200

    feat(Sample Thymeleaf View): Add a parameter to the template

--- src/main/kotlin/com/ssi/verifier/webapp/SampleViewController.kt
+++ src/main/kotlin/com/ssi/verifier/webapp/SampleViewController.kt
@@ -3,6 +3,7 @@ package com.ssi.verifier.webapp
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 
@@ -13,7 +14,8 @@ class SampleViewController {
 
     @ApiOperation(value = "Returns a simple string")
     @GetMapping("", produces = ["text/plain"])
-    fun sample(): String {
+    fun sample(model: Model): String {
+        model.addAttribute("test", "Hello, world!")
         return "sample"
     }
 }
