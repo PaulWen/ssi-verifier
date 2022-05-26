438553644790fe54ab0f1d0d9e15a01e858711ab
PaulWen <wenzel.pgb@googlemail.com>
2022-05-26 15:41:43 +0200

    feat(Lissi Agent): Render connectionless proof request as QR code

--- src/main/kotlin/com/ssi/verifier/domain/models/ConnectionlessProofRequest.kt
+++ src/main/kotlin/com/ssi/verifier/domain/models/ConnectionlessProofRequest.kt
@@ -1,6 +1,28 @@
 package com.ssi.verifier.domain.models
 
-data class ConnectionlessProofRequest(
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.MultiFormatWriter
+import com.google.zxing.client.j2se.MatrixToImageWriter
+import java.io.ByteArrayOutputStream
+import java.util.*
+
+
+class ConnectionlessProofRequest(
     val exchangeId: String,
     val url: String
-)
+) {
+    val qrCodeDataUrlEncoded: String
+
+    init {
+        val imageSize = 800
+        val matrix = MultiFormatWriter().encode(url, BarcodeFormat.QR_CODE,
+            imageSize, imageSize)
+        val bos = ByteArrayOutputStream()
+        MatrixToImageWriter.writeToStream(matrix, "png", bos)
+        val image: String = Base64.getEncoder().encodeToString(bos.toByteArray())
+
+        qrCodeDataUrlEncoded = "data:image/png;base64,$image"
+    }
+
+}
+
