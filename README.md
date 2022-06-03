38674cb836a3feb8f846f5c86ad06852faec3b76
PaulWen <wenzel.pgb@googlemail.com>
2022-06-03 07:18:46 +0200

    feat: only subscribe to updates for the current proof request and use Thymeleaf to render the WebSocket message

--- README.md
+++ README.md
@@ -11,3 +11,26 @@ A simple SSI verifier app that integrates with the Lissi Agent.
   a manual restart of the application
 - add the webhook URL when starting the
   AcaPy `--webhook-url 'http://host.docker.internal:8888/api/acapy-webhook' \`
+
+## Technical Debt
+
+- AcaPy Webhook Endpoint muss geschützt werden, da ein Angreifer ansonsten AcaPy
+  imitieren kann und uns falsche Daten schicken kann - nur der AcaPy sollte die
+  Webhooks aufrufen können!!
+- WebSockets mit authorization Header absichern, damit nicht jeder subscriben
+  kann
+- Socket stream html element sollte nur eine Connection aufbauen und dann zu
+  vielen topics subscriben anstatt jedes Mal eine neue Connection aufzubauen
+- store proof results in-memory and offer a reload button in case the WebSocket
+  message got lost
+
+## Next Steps
+
+- only sent the verification result
+- also sent the proof claims
+- replace the QR Code with the result
+- make everything look nice and be responsive using a UI Library
+
+- introduce a proof template overview page where the client can choose the proof
+  template from he wants to request connectionless
+- show images to each proof template
--- src/main/kotlin/com/ssi/verifier/outbound/services/WebSocketNotificationService.kt
+++ src/main/kotlin/com/ssi/verifier/outbound/services/WebSocketNotificationService.kt
@@ -4,25 +4,19 @@ import com.ssi.verifier.domain.models.ProofExchangeRecordDo
 import com.ssi.verifier.domain.services.NotificationService
 import org.springframework.messaging.simp.SimpMessagingTemplate
 import org.springframework.stereotype.Service
+import org.thymeleaf.context.Context
+import org.thymeleaf.spring5.SpringTemplateEngine
 
 @Service
 class WebSocketNotificationService(
     private val wsService: SimpMessagingTemplate,
+    private val templateEngine: SpringTemplateEngine
 ) : NotificationService {
-    override fun proofExchangeUpdate(proofExchangeRecordDo: ProofExchangeRecordDo) {
-        val htmlUpdate = """
-            <turbo-stream action="append" target="proof-exchange-update">
-                <template>
-                    <div>
-                        ID: ${proofExchangeRecordDo.id}
-                        State: ${proofExchangeRecordDo.state}
-                        Verified: ${proofExchangeRecordDo.isVerified}
-                        Valid: ${proofExchangeRecordDo.isValid}
-                    </div>
-                </template>
-            </turbo-stream>
-        """
+    override fun proofExchangeUpdate(proofExchangeRecord: ProofExchangeRecordDo) {
+        val context = Context()
+        context.setVariable("proofExchangeRecord", proofExchangeRecord)
+        val htmlUpdate = templateEngine.process("proof-exchange-update", context)
 
-        wsService.convertAndSend("/proof", htmlUpdate)
+        wsService.convertAndSend("/proof/${proofExchangeRecord.id}", htmlUpdate)
     }
 }
