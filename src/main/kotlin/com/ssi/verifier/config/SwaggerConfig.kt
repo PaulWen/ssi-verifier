f5c6d2c55680a4d20b7fc9c61315d3a8011e8cd5
PaulWen <wenzel.pgb@googlemail.com>
2022-09-24 16:45:35 +0200

    feat(Monitoring): Expose actuator under a separate port 8080 than the application itself

--- src/main/kotlin/com/ssi/verifier/config/SwaggerConfig.kt
+++ src/main/kotlin/com/ssi/verifier/config/SwaggerConfig.kt
@@ -1,20 +1,7 @@
 package com.ssi.verifier.config
 
-import org.springframework.boot.actuate.autoconfigure.endpoint.web.CorsEndpointProperties
-import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties
-import org.springframework.boot.actuate.autoconfigure.web.server.ManagementPortType
-import org.springframework.boot.actuate.endpoint.ExposableEndpoint
-import org.springframework.boot.actuate.endpoint.web.EndpointLinksResolver
-import org.springframework.boot.actuate.endpoint.web.EndpointMapping
-import org.springframework.boot.actuate.endpoint.web.EndpointMediaTypes
-import org.springframework.boot.actuate.endpoint.web.WebEndpointsSupplier
-import org.springframework.boot.actuate.endpoint.web.annotation.ControllerEndpointsSupplier
-import org.springframework.boot.actuate.endpoint.web.annotation.ServletEndpointsSupplier
-import org.springframework.boot.actuate.endpoint.web.servlet.WebMvcEndpointHandlerMapping
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.env.Environment
-import org.springframework.util.StringUtils
 import springfox.documentation.builders.ApiInfoBuilder
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
@@ -54,44 +41,4 @@ class SwaggerConfig {
             .build()
     }
 
-    // This bean resolves problem with actuator and swagger
-    // https://github.com/springfox/springfox/issues/3462#issuecomment-1010721223
-    @Bean
-    fun webEndpointServletHandlerMapping(
-        webEndpointsSupplier: WebEndpointsSupplier,
-        servletEndpointsSupplier: ServletEndpointsSupplier,
-        controllerEndpointsSupplier: ControllerEndpointsSupplier,
-        endpointMediaTypes: EndpointMediaTypes?,
-        corsProperties: CorsEndpointProperties,
-        webEndpointProperties: WebEndpointProperties,
-        environment: Environment
-    ): WebMvcEndpointHandlerMapping? {
-        val allEndpoints: MutableList<ExposableEndpoint<*>?> = ArrayList()
-        val webEndpoints = webEndpointsSupplier.endpoints
-        allEndpoints.addAll(webEndpoints)
-        allEndpoints.addAll(servletEndpointsSupplier.endpoints)
-        allEndpoints.addAll(controllerEndpointsSupplier.endpoints)
-        val basePath = webEndpointProperties.basePath
-        val endpointMapping = EndpointMapping(basePath)
-        val shouldRegisterLinksMapping = shouldRegisterLinksMapping(webEndpointProperties, environment, basePath)
-        return WebMvcEndpointHandlerMapping(
-            endpointMapping,
-            webEndpoints,
-            endpointMediaTypes,
-            corsProperties.toCorsConfiguration(),
-            EndpointLinksResolver(allEndpoints, basePath),
-            shouldRegisterLinksMapping,
-            null
-        )
-    }
-
-    private fun shouldRegisterLinksMapping(
-        webEndpointProperties: WebEndpointProperties,
-        environment: Environment,
-        basePath: String
-    ): Boolean {
-        return webEndpointProperties.discovery.isEnabled && (StringUtils.hasText(basePath) || ManagementPortType.get(
-            environment
-        ) == ManagementPortType.DIFFERENT)
-    }
 }
